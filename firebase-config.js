bae6debbabd69405238d31ffe82bc3a5976e7a13
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-24 13:49:04 +0530

    Chat app

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlSN0__lD7GH0cDvqQQq9nX2gn1TRRXlE",
+  authDomain: "raviranjanchat-10f42.firebaseapp.com",
+  projectId: "raviranjanchat-10f42",
+  storageBucket: "raviranjanchat-10f42.firebasestorage.app",
+  messagingSenderId: "845872620022",
+  appId: "1:845872620022:web:bc929300410e45af3fa7d7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
